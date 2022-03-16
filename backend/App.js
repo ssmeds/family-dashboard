36d2e12d357161ad721cfe9ecad016fefb2be4ef
ssmeds <stina_smeds@hotmail.com>
2022-03-16 09:55:53 +0100

    added backend and frontend directories, added connection to database

---
+++ backend/App.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = 3000
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+// uuidv4();
+
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const userSchema = new mongoose.Schema({
+  userId: String,
+  firstName: String,
+  lastName: String,
+  roll: String,
+  familyMembers: [String]
+})
+
+const User = mongoose.model('User', userSchema)
+
+const user = new User({
+  userId: uuidv4(),
+  firstName: 'Stintan',
+  lastName: 'Smedsan',
+  roll: 'Parent',
+  familyMembers: ['Fredrik', 'Johannes']
+});
+user.save().then(() => { console.log('One entry added'); })
+
+app.get('/', (req, res) => {
+  User.find({}, (found, err) => {
+    if (!err) {
+      res.send(found);
+    }
+    console.log('this is the err', err);
+    res.send("Some error occured!")
+  })
+});
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World! This is your backend!')
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is your front end!</h1>
+    </div>
+  );
+}
+
+export default App;
